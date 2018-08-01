c65520c153b09ceccf2b93fb984f41785aeee2ab
William Lu <williamlu2015@alumni.ubc.ca>
2018-08-01 08:46:26 -0700

    Removed normalize function

--- contentScript.js
+++ contentScript.js
@@ -1,26 +1,3 @@
-function normalizeUrl() {
-    let url = location.href;
-    let array = url.split("/");
-    let index = array.indexOf("Special:ImagefromIndex");
-
-    if (index === -1) {
-        return;
-    }
-
-    if (index + 1 >= array.length) {
-        return;
-    }
-
-    let newUrl = array.slice(0, index + 1).join("/") + "/" + array[index + 1];
-
-    if (newUrl === url) {
-        return false;
-    } else {
-        location.replace(newUrl);
-        return true;
-    }
-}
-
 function acceptDisclaimer() {
     let target = "I accept this disclaimer, continue to download file";
 
@@ -38,17 +15,19 @@ function removeWait() {
     let waitElement = document.getElementById("sm_dl_wait");
 
     if (waitElement === null) {
-        return;
+        return false;
     }
 
     let url = waitElement.getAttribute("data-id");
     location.replace(url);
+    return true;
 }
 
 (function() {
     if (acceptDisclaimer()) {
         return;
     }
-
-    removeWait();
+    if (removeWait()) {
+        return;
+    }
 })();
